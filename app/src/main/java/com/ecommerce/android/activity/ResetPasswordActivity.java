1e856d2b7c49b64c4e4e688ce0b5894e68a82cb4
gouravpal1991 <gouravpal1991@gmail.com>
2017-02-03 10:06:52 +0530

    first commit

---
+++ app/src/main/java/com/ecommerce/android/activity/ResetPasswordActivity.java
@@ -0,0 +1,18 @@
+package com.ecommerce.android.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.ecommerce.android.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+    }
+}
